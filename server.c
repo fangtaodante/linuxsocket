c08646495c0200252481eaa5c5b8f00efbc4fb7d
Dante <fangtao_dante@126.com>
2019-06-02 20:45:55 +0800

    多线程服务器修改

--- server.c
+++ server.c
@@ -6,17 +6,46 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>//sockert_addr_in
 #include <ctype.h>//toupper
-#include <sys/wait.h>
-#include <signal.h>
+#include <pthread.h>
 
 #include "wrap.h"
 
-#define SERV_IP "127.0.0.1"
+#define MAXLINE 80
 #define SERV_PORT 1214
 
-void wait_child(int signo){
-    while(waitpid(0, NULL, WNOHANG)>0)
-    return;
+struct s_info{
+    struct sockaddr_in cliaddr;
+    int cfd;
+};
+
+
+void *do_work(void *arg){
+    int n,i;
+    struct s_info *ts = (struct s_info*)arg;
+    char buf[MAXLINE];
+    char str[INET_ADDRSTRLEN];
+    pthread_detach(pthread_self());//分离进程，就不用回收了
+    while(1){
+        n = Read(ts->cfd,buf,sizeof(buf));
+        if(n==0){
+            printf("The other side has been closed.\n");
+            return 0;
+        }else if(n==-1){
+            perror("Read error");
+            exit(1);
+        }else{
+            printf("Receive from %s at Port %d.\n",
+            inet_ntop(AF_INET,&(*ts).cliaddr.sin_addr.s_addr,str,sizeof(str)),
+            ntohs((*ts).cliaddr.sin_port));
+            for(i=0;i<n;i++)
+                buf[i]=toupper(buf[i]);
+            Write(ts->cfd,buf,n);
+            Write(STDOUT_FILENO,buf,n);
+        }
+        Close(ts->cfd);
+
+        return (void *)0;
+    }
 }
 
 int main(){
@@ -24,8 +53,9 @@ int main(){
     struct sockaddr_in serv_addr,clie_addr;
     socklen_t clie_addr_len;
     char buf[BUFSIZ],clie_IP[BUFSIZ];
-    int n,i;
-    pid_t pid;
+    int i=0;
+    pthread_t tid;
+    struct s_info ts[256];
     fd_listen = Socket(AF_INET,SOCK_STREAM,0);
     //创建一个socket描述符，成功返回文件描述符，失败返回 -1；
     bzero(&serv_addr,sizeof(serv_addr));
@@ -39,39 +69,16 @@ int main(){
     //指定监听上限数，同时允许多少客户端建立连接；
     printf("Accepting connections...\n");
 
-    //建立多进程服务器,父进程不断循环创建子进程
+    //建立多线程服务器
     while(1){
         clie_addr_len=sizeof(clie_addr);
         fd_connect = Accept(fd_listen,(struct sockaddr*)&clie_addr, &clie_addr_len);
-        //刻在脑子里，接收连接请求，clie_addr:传出参数，addr_len:传入传出参数，返回一个新的文件描述符，用于通信
+        //接收连接请求，clie_addr:传出参数，addr_len:传入传出参数，返回一个新的文件描述符，用于通信
         //阻塞等待客户端发起连接
-        printf("Client IP:%s, Port:%d\n",inet_ntop(AF_INET, &clie_addr.sin_addr.s_addr,clie_IP,sizeof(clie_IP)),
-        ntohs(clie_addr.sin_port));
-        pid = Fork();
-        if(pid == 0){//子进程
-            close(fd_listen);
-            break;
-        } else {
-            close(fd_connect);
-            signal(SIGCHLD,wait_child);
-        }
+        ts[i].cliaddr=clie_addr;
+        ts[i].cfd=fd_connect;
+        pthread_create(&tid,NULL,do_work,(void*)&ts[i]);
+        i++;
     }
-    if(pid==0){//子进程
-        while(1){
-            n = Read(fd_connect,buf,sizeof(buf));
-            if(n==0){
-                close(fd_connect);
-                return 0;
-            }else if(n==-1){
-                perror("Read error");
-                exit(1);
-            }else{
-                for(i=0;i<n;i++)
-                buf[i]=toupper(buf[i]);
-                Write(fd_connect,buf,n);
-                Write(STDOUT_FILENO,buf,n);
-            }
-        }
-    }
-
+    return 0;
 }
